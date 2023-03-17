29020acfdb616b23a8a4cf4c158b0c4b9d7f0203
ZakColonel <zakariaanbouri1999@gmail.Com>
2023-03-17 04:17:56 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * main function returns a string
+ * main - function returns a string
+ * return: 0 if all good
 **/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
